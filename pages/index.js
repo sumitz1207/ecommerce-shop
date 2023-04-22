03ff45c2981b40732d3d373f9d79ad0b98e540b9
Sumit Patel <sumitz1207@gmail.com>
2023-04-21 22:40:27 -0400

    login page initialized

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,6 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className={'bg-red-500'}>words</div>
+    <div className="bg-blue-900 w-screen h-screen flex items-center">words</div>
   )
 }

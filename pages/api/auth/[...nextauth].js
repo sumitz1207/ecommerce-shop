3275ff43944ca83b958cfdaf2d6d66a519c5882f
Sumit Patel <sumitz1207@gmail.com>
2023-04-23 23:26:14 -0400

    Add mongodb adapter to nextauth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,6 @@
 import NextAuth from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
+import { MongoDBAdapter } from '@next-auth/mongodb-adapter'
 
 export default NextAuth({
   providers: [
@@ -7,5 +8,7 @@ export default NextAuth({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET
     }),
-  ]
+  ],
+  adapter: MongoDBAdapter
+
 })
